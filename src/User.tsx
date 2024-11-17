588ea65d3ce8ad06c9197e7e9cc55836e5d87ff7
Shivang Jain <129207095+shivangjain03@users.noreply.github.com>
2024-11-17 06:47:04 -0800

    Update User.tsx

--- src/User.tsx
+++ src/User.tsx
@@ -92,7 +92,7 @@ const ExperienceInfo = [
     {
         role: "Web Developer",
         company: "CJSF 90.1FM",
-        date: "May 2023 - Present",
+        date: "April 2023 - November 2024",
         desc: "I spearheaded the migration of the organization's website from Drupal 7 to Drupal 8, leveraging my expertise in PHP, HTML5, CSS3, JavaScript and Drupal to ensure a seamless and efficient transition.",
         skills: ["HTML", "CSS", "React", "JavaScript", "Drupal", "Docker", "TypeSript"]
     }
